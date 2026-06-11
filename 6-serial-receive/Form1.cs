23c55207e426ddff56ed545a9ad64cad6f71ca05
Linho <linho1219@outlook.com>
2026-06-11 16:33:09 +0800

    串口空白项目

---
+++ 6-serial-receive/Form1.cs
@@ -0,0 +1,10 @@
+namespace _6_serial_receive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 6-serial-send/Form1.cs
@@ -0,0 +1,10 @@
+namespace _6_serial_send
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
